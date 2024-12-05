78f2f1cd475bca0a6e718707b3662c11f3f6712d
Tom Bee <tburzynski2@yahoo.com>
2024-12-05 16:56:44 -0600

    Imported fonts; Basic nav bar structure

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".navbar__hamburger");
+  const navbar = document.querySelector(".navbar");
+
+  hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("navbar--open");
+  });
+
+  const dropdownToggles = document.querySelectorAll(".dropdown-toggle");
+  dropdownToggles.forEach((toggle) => {
+    toggle.addEventListener("click", (e) => {
+      e.preventDefault();
+      const dropdownMenu = toggle.nextElementSibling;
+      dropdownMenu.classList.toggle("open");
+    });
+  });
+});
