c98bd20b4b127fc05558d6edcdddeef9871a4dd8
Tom Bee <tburzynski2@yahoo.com>
2024-12-29 14:46:32 -0600

    Code cleanup pt.5 - Fixing BEM naming inconsistencies

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
-  const headerButton = document.querySelector(".btn__nav-quote");
+  const headerButton = document.querySelector(".btn--nav-quote");
   if (window.scrollY > 50) {
     header.classList.add("shrink");
     headerButton.classList.add("shrink");
